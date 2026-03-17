a8de15ff30127c602dcc4e5575d4580dc4c6ca7e
DoanhTran-Sotatek <doanh.tran@sotatek.com>
2026-03-17 14:35:16 +0700

    chore: update .gitignore and add app_networking module

---
+++ app_networking/lib/app_networking.dart
@@ -0,0 +1,7 @@
+export 'src/config/network_config.dart';
+export 'src/client/api_client.dart';
+export 'src/interceptor/error_interceptor.dart';
+export 'src/interceptor/logging_interceptor.dart';
+export 'src/di/networking_di_module.dart';
+export 'src/di/networking_package_module.dart';
+export 'src/di/networking_package_module.module.dart';
